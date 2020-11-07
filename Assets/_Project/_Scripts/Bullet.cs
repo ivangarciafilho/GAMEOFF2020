099b0176895dd04347cc75c10d5d710f6faf4673
Elvis L <elvis.leite.9@gmail.com>
2020-11-06 23:12:24 -0300

    basic shooting

---
+++ Assets/_Project/_Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	public float speed = 10.0f;
+
+	void Start()
+	{
+		Destroy(gameObject, 2.0f);
+	}
+
+    void Update()
+    {
+	    transform.position += transform.right * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/_Project/_Scripts/Gun.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+	public Bullet bullet;
+	public Transform firePoint;
+	public float shootRate = 0.25f;
+	float shootRateElapsed = 0.0f;
+	
+    void Start()
+    {
+	    shootRateElapsed = Time.time + shootRate;
+    }
+
+    void Update()
+    {
+	    Vector2 dir = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+	    float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+	    
+	    transform.rotation = Quaternion.Euler(0, 0, angle);
+	    
+	    if(Input.GetButtonDown("Fire1") && shootRateElapsed < Time.time)
+	    {
+	    	var bulletInstance = Instantiate(bullet, firePoint.position, firePoint.rotation);
+	    	
+	    	shootRateElapsed = Time.time + shootRate;
+	    }
+    }
+    
+	// Implement OnDrawGizmos if you want to draw gizmos that are also pickable and always drawn.
+	protected void OnDrawGizmos()
+	{
+		if(firePoint != null)
+		{
+		Gizmos.color = Color.red;
+			Gizmos.DrawLine(firePoint.position, firePoint.position + firePoint.right);
+		}
+	}
+}
